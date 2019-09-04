a579b26f3719e8d304487d9b516524a150c8ade3
Alex Tong <alexanderytong@gmail.com>
2019-09-04 08:44:01 -0400

    Partially working growth function

--- train_scrna.py
+++ train_scrna.py
@@ -7,13 +7,15 @@ import os
 import time
 
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 
 import lib.utils as utils
 from lib.visualize_flow import visualize_transform
 import lib.layers.odefunc as odefunc
 
-from train_misc import standard_normal_logprob
+from train_misc import standard_normal_logprob, uniform_logprob
 from train_misc import set_cnf_options, count_nfe, count_parameters, count_total_time
 from train_misc import add_spectral_norm, spectral_norm_power_iteration
 from train_misc import create_regularization_fns, get_regularization, append_regularization_to_log
@@ -38,6 +40,7 @@ parser.add_argument('--train_T', type=eval, default=True)
 parser.add_argument("--divergence_fn", type=str, default="brute_force", choices=["brute_force", "approximate"])
 parser.add_argument("--nonlinearity", type=str, default="tanh", choices=odefunc.NONLINEARITIES)
 
+parser.add_argument('--alpha', type=float, default=0.0, help="loss weight parameter for growth model")
 parser.add_argument('--solver', type=str, default='dopri5', choices=SOLVERS)
 parser.add_argument('--atol', type=float, default=1e-5)
 parser.add_argument('--rtol', type=float, default=1e-5)
@@ -140,12 +143,7 @@ def val_sampler(i):
 def viz_sampler(i):
     return inf_sampler(data[labels==i], batch_size=args.viz_batch_size)
 
-
-
 full_sampler = lambda: inf_sampler(data, 2000)
-#train_samplers = [lambda: inf_sampler(data[labels==i], noise = 0.1) for i in timepoints]
-#val_samplers = [lambda: inf_sampler(data[labels==i], args.test_batch_size) for i in timepoints]
-#viz_samplers = [lambda: inf_sampler(data[labels==i], args.viz_batch_size) for i in timepoints]
 
 def scatter_timepoints():
     import matplotlib.pyplot as plt
@@ -171,13 +169,6 @@ def scatter_timepoints():
     plt.close()
 
 scatter_timepoints()
-"""
-from torch.utils.data import TensorDataset, DataLoader, RandomSampler, BatchSampler
-datasets = [TensorDataset(data[labels==i]) for i in classes]
-print(classes)
-data_sampler = [BatchSampler(RandomSampler(d, replacement=True), args.batch_size, False) for d in datasets]
-data_loader = [DataLoader(d, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True, sampler=s) for d,s in zip(datasets, data_sampler)]
-"""
 
 def get_transforms(model, integration_times):
     """
@@ -211,7 +202,7 @@ def get_transforms(model, integration_times):
     return sample_fn, density_fn
 
 
-def compute_loss(args, model):
+def compute_loss(args, model, growth_model):
     """
     Compute loss by integrating backwards from the last time step
     At each time step integrate back one time step, and concatenate that
@@ -219,144 +210,185 @@ def compute_loss(args, model):
     repeating over and over to calculate the likelihood of samples in
     later timepoints iteratively, making sure that the ODE is evaluated
     at every time step to calculate those later points.
+
+    The growth model is a single model of time independent cell growth / 
+    death rate defined as a variation from uniform.
     """
+
+    # Backward pass accumulating losses, previous state and deltas
     deltas = []
+    xs = []
+    #prev_xs = []
     for i, (itp, tp) in enumerate(zip(int_tps[::-1], timepoints[::-1])): # tp counts down from last
         integration_times = torch.tensor([itp-args.time_length, itp]).type(torch.float32).to(device)
 
         # load data
         x = train_sampler(tp)
         x = torch.from_numpy(x).type(torch.float32).to(device)
+        xs.append(x)
         if i > 0:
             x = torch.cat((z, x))
         zero = torch.zeros(x.shape[0], 1).to(x)
 
         # transform to previous timepoint
         z, delta_logp = model(x, zero, integration_times=integration_times)
+        #prev_xs.append(z[-args.batch_size:])
         deltas.append(delta_logp)
 
-    # compute log q(z)
-    logpz = standard_normal_logprob(z).sum(1, keepdim=True)
+    # compute log growth probability
+    xs = torch.cat(xs)
+    #growth_zs, growth_delta_logps = growth_model(xs, torch.zeros(xs.shape[0], 1).to(xs)) # Use default timestep
+    #growth_logpzs = uniform_logprob(growth_zs).sum(1, keepdim=True)
+    #growth_logpzs = standard_normal_logprob(growth_zs).sum(1, keepdim=True)
+    #growth_logpxs = growth_logpzs - growth_delta_logps
 
+    # compute log q(z) with forward pass
+    logpz = standard_normal_logprob(z).sum(1, keepdim=True)
     logps = [logpz]
     losses = []
     for delta_logp in deltas[::-1]:
         logpx = logps[-1] - delta_logp
         logps.append(logpx[:-args.batch_size])
         losses.append(-torch.mean(logpx[-args.batch_size:]))
-    #weights = torch.tensor([0,0,0,0,1]).to(logpx)
-    #weights = torch.tensor([1,0,0,0,0]).to(logpx)
-    #weights = torch.tensor([1,1,1,1,1]).to(logpx)
+    # weights = torch.tensor([1,1,1,1,1]).to(logpx)
     weights = torch.tensor([5,4,3,2,1]).to(logpx)
-    loss = torch.sum(torch.stack(losses) * weights)
-    return loss
+    losses = torch.stack(losses)
 
+    losses = torch.mean(losses * weights)
 
-if __name__ == '__main__':
-    regularization_fns, regularization_coeffs = create_regularization_fns(args)
-    model = build_model_tabular(args, 2, regularization_fns).to(device)
-    if args.spectral_norm: add_spectral_norm(model)
-    set_cnf_options(args, model)
+    #growth_losses = -torch.mean(growth_logpxs)
+    #alpha = torch.tensor(args.alpha).to(growth_losses)
+    #loss = (1 - alpha) * losses + alpha * growth_losses
+    #loss = losses + growth_losses
+    return losses#, growth_losses
+    #return loss
 
-    if args.test:
-        model.load_state_dict(torch.load(args.save + '/checkpt.pth')['state_dict'])
-    else:
-        logger.info(model)
-        logger.info("Number of trainable parameters: {}".format(count_parameters(model)))
+def train(args, model, growth_model):
+    logger.info(model)
+    logger.info("Number of trainable parameters: {}".format(count_parameters(model)))
 
-        optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    optimizer = optim.Adam(set(model.parameters()) | set(growth_model.parameters()), 
+                           lr=args.lr, weight_decay=args.weight_decay)
+    #growth_optimizer = optim.Adam(growth_model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 
-        time_meter = utils.RunningAverageMeter(0.93)
-        loss_meter = utils.RunningAverageMeter(0.93)
-        nfef_meter = utils.RunningAverageMeter(0.93)
-        nfeb_meter = utils.RunningAverageMeter(0.93)
-        tt_meter = utils.RunningAverageMeter(0.93)
+    time_meter = utils.RunningAverageMeter(0.93)
+    loss_meter = utils.RunningAverageMeter(0.93)
+    nfef_meter = utils.RunningAverageMeter(0.93)
+    nfeb_meter = utils.RunningAverageMeter(0.93)
+    tt_meter = utils.RunningAverageMeter(0.93)
 
-        end = time.time()
-        best_loss = float('inf')
-        model.train()
-        for itr in range(1, args.niters + 1):
-            optimizer.zero_grad()
+    end = time.time()
+    best_loss = float('inf')
+    model.train()
+    growth_model.train()
+    for itr in range(1, args.niters + 1):
+        optimizer.zero_grad()
+        #growth_optimizer.zero_grad()
 
-            ### Train
-            if args.spectral_norm: spectral_norm_power_iteration(model, 1)
+        ### Train
+        if args.spectral_norm: spectral_norm_power_iteration(model, 1)
+        if args.spectral_norm: spectral_norm_power_iteration(growth_model, 1)
 
-            loss = compute_loss(args, model)
-            loss_meter.update(loss.item())
+        loss = compute_loss(args, model, growth_model)
+        #loss, loss2 = compute_loss(args, model, growth_model)
+        loss_meter.update(loss.item())
 
-            if len(regularization_coeffs) > 0 and tp == timepoints[-1]:
-                # Only regularize on the last timepoint
-                reg_states = get_regularization(model, regularization_coeffs)
-                reg_loss = sum(
-                    reg_state * coeff for reg_state, coeff in zip(reg_states, regularization_coeffs) if coeff != 0
-                )
-                loss = loss + reg_loss
-
-            total_time = count_total_time(model)
-            nfe_forward = count_nfe(model)
-
-            loss.backward()
-            optimizer.step()
-
-            ### Eval
-            nfe_total = count_nfe(model)
-            nfe_backward = nfe_total - nfe_forward
-            nfef_meter.update(nfe_forward)
-            nfeb_meter.update(nfe_backward)
-
-            time_meter.update(time.time() - end)
-            tt_meter.update(total_time)
-
-            log_message = (
-                'Iter {:04d} | Time {:.4f}({:.4f}) | Loss {:.6f}({:.6f}) | NFE Forward {:.0f}({:.1f})'
-                ' | NFE Backward {:.0f}({:.1f}) | CNF Time {:.4f}({:.4f})'.format(
-                    itr, time_meter.val, time_meter.avg, loss_meter.val, loss_meter.avg, nfef_meter.val, nfef_meter.avg,
-                    nfeb_meter.val, nfeb_meter.avg, tt_meter.val, tt_meter.avg
-                )
+        if len(regularization_coeffs) > 0:
+            # Only regularize on the last timepoint
+            reg_states = get_regularization(model, regularization_coeffs)
+            reg_loss = sum(
+                reg_state * coeff for reg_state, coeff in zip(reg_states, regularization_coeffs) if coeff != 0
             )
-            if len(regularization_coeffs) > 0:
-                log_message = append_regularization_to_log(log_message, regularization_fns, reg_states)
+            loss = loss + reg_loss
 
-            logger.info(log_message)
+        if len(growth_regularization_coeffs) > 0:
+            growth_reg_states = get_regularization(growth_model, growth_regularization_coeffs)
+            reg_loss = sum(
+                reg_state * coeff for reg_state, coeff in zip(growth_reg_states, growth_regularization_coeffs) if coeff != 0
+            )
+            loss2 = loss2 + reg_loss
 
-            if itr % args.val_freq == 0 or itr == args.niters:
-                with torch.no_grad():
-                    model.eval()
-                    test_loss = compute_loss(args, model)
-                    test_nfe = count_nfe(model)
-                    log_message = '[TEST] Iter {:04d} | Test Loss {:.6f} | NFE {:.0f}'.format(itr, test_loss, test_nfe)
-                    logger.info(log_message)
+        total_time = count_total_time(model)
+        nfe_forward = count_nfe(model)
 
-                    if test_loss.item() < best_loss:
-                        best_loss = test_loss.item()
-                        utils.makedirs(args.save)
-                        torch.save({
-                            'args': args,
-                            'state_dict': model.state_dict(),
-                        }, os.path.join(args.save, 'checkpt.pth'))
-                    model.train()
+        loss.backward()
+        #loss2.backward()
+        optimizer.step()
+        #growth_optimizer.step()
 
-            if itr % args.viz_freq == 0:
-                with torch.no_grad():
-                    model.eval()
-                    for i, _ in enumerate(timepoints):
-                        p_samples = viz_sampler(i)
-                        sample_fn, density_fn = get_transforms(model, int_tps[:i+1])
-                        plt.figure(figsize=(9, 3))
-                        visualize_transform(
-                            p_samples, torch.randn, standard_normal_logprob, transform=sample_fn, inverse_transform=density_fn,
-                            samples=True, npts=800, device=device
-                        )
-                        fig_filename = os.path.join(args.save, 'figs', '{:04d}_{:01d}.jpg'.format(itr, i))
-                        utils.makedirs(os.path.dirname(fig_filename))
-                        plt.savefig(fig_filename)
-                        plt.close()
-                    model.train()
+        ### Eval
+        nfe_total = count_nfe(model)
+        nfe_backward = nfe_total - nfe_forward
+        nfef_meter.update(nfe_forward)
+        nfeb_meter.update(nfe_backward)
+        time_meter.update(time.time() - end)
+        tt_meter.update(total_time)
 
-            end = time.time()
+        log_message = (
+            'Iter {:04d} | Time {:.4f}({:.4f}) | Loss {:.6f}({:.6f}) | NFE Forward {:.0f}({:.1f})'
+            ' | NFE Backward {:.0f}({:.1f}) | CNF Time {:.4f}({:.4f})'.format(
+                itr, time_meter.val, time_meter.avg, loss_meter.val, loss_meter.avg, nfef_meter.val, nfef_meter.avg,
+                nfeb_meter.val, nfeb_meter.avg, tt_meter.val, tt_meter.avg
+            )
+        )
+        if len(regularization_coeffs) > 0:
+            log_message = append_regularization_to_log(log_message, regularization_fns, reg_states)
 
-        logger.info('Training has finished.')
+        logger.info(log_message)
 
+        if itr % args.val_freq == 0 or itr == args.niters:
+            with torch.no_grad():
+                model.eval()
+                growth_model.eval()
+                test_loss = compute_loss(args, model, growth_model)
+                test_nfe = count_nfe(model)
+                log_message = '[TEST] Iter {:04d} | Test Loss {:.6f} | NFE {:.0f}'.format(itr, test_loss, test_nfe)
+                logger.info(log_message)
+
+                if test_loss.item() < best_loss:
+                    best_loss = test_loss.item()
+                    utils.makedirs(args.save)
+                    torch.save({
+                        'args': args,
+                        'state_dict': model.state_dict(),
+                    }, os.path.join(args.save, 'checkpt.pth'))
+                model.train()
+
+        if itr % args.viz_freq == 0:
+            with torch.no_grad():
+                model.eval()
+                for i, _ in enumerate(timepoints):
+                    p_samples = viz_sampler(i)
+                    sample_fn, density_fn = get_transforms(model, int_tps[:i+1])
+                    #growth_sample_fn, growth_density_fn = get_transforms(growth_model, int_tps[:i+1])
+                    plt.figure(figsize=(9, 3))
+                    visualize_transform(
+                        p_samples, torch.randn, standard_normal_logprob, transform=sample_fn, inverse_transform=density_fn,
+                        samples=True, npts=800, device=device
+                    )
+                    fig_filename = os.path.join(args.save, 'figs', '{:04d}_{:01d}.jpg'.format(itr, i))
+                    utils.makedirs(os.path.dirname(fig_filename))
+                    plt.savefig(fig_filename)
+                    plt.close()
+
+                    # Visualize growth transform
+                    #visualize_transform(
+                    #    p_samples, torch.rand, uniform_logprob, transform=growth_sample_fn, 
+                    #    inverse_transform=growth_density_fn,
+                    #    samples=True, npts=800, device=device
+                    #)
+
+                    #fig_filename = os.path.join(args.save, 'growth_figs', '{:04d}_{:01d}.jpg'.format(itr, i))
+                    #utils.makedirs(os.path.dirname(fig_filename))
+                    #plt.savefig(fig_filename)
+                    #plt.close()
+
+                model.train()
+        end = time.time()
+    logger.info('Training has finished.')
+
+
+def plot_output(args, model):
     save_traj_dir = os.path.join(args.save, 'trajectory')
     logger.info('Plotting trajectory to {}'.format(save_traj_dir))
     data_samples = full_sampler()
@@ -367,3 +399,38 @@ if __name__ == '__main__':
     save_traj_dir2 = os.path.join(args.save, 'trajectory_to_end')
     save_trajectory(model, data_samples, save_traj_dir2, device=device, end_times=[int_tps[-1]], ntimes=25)
     trajectory_to_video(save_traj_dir2)
+
+class GrowthNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.fc1 = nn.Linear(2,64)
+        self.fc2 = nn.Linear(64,64)
+        self.fc3 = nn.Linear(64,2)
+
+    def forward(self, x):
+        x = F.tanh(self.fc1(x))
+        x = F.tanh(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+if __name__ == '__main__':
+    regularization_fns, regularization_coeffs = create_regularization_fns(args)
+    growth_regularization_fns, growth_regularization_coeffs = create_regularization_fns(args)
+    model = build_model_tabular(args, 2, regularization_fns).to(device)
+    #growth_model = GrowthNet().to(device)
+    growth_model = build_model_tabular(args, 2, growth_regularization_fns).to(device)
+    if args.spectral_norm: add_spectral_norm(model)
+    if args.spectral_norm: add_spectral_norm(growth_model)
+    set_cnf_options(args, model)
+    set_cnf_options(args, growth_model)
+
+    if args.test:
+        model.load_state_dict(torch.load(args.save + '/checkpt.pth')['state_dict'])
+        growth_model.load_state_dict(torch.load(args.save + '/checkpt.pth')['growth_state_dict'])
+    else:
+        train(args, model, growth_model)
+
+    plot_output(args, model, growth_model)
+
+
